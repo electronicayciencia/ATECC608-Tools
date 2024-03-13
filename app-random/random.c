72c8fc55b35ab93f7c042b7a119d7beb113ccf79
Electronicayciencia <electronicayciencia@gmail.com>
2024-03-13 13:03:33 +0100

    Clean examples

--- app-random/random.c
+++ app-random/random.c
@@ -3,26 +3,15 @@
 
 #include <stdio.h>
 #include "cryptoauthlib.h"
+#include "atca_config.h"
 
 int main(void) {
     uint8_t random_number[32];
     uint8_t buffer[1024];
     size_t buffer_size = sizeof(buffer);
 
-    ATCAIfaceCfg mycfg_i2c = {
-        .iface_type                 = ATCA_I2C_IFACE,
-        .devtype                    = ATECC608,
-        {
-            .atcai2c.address        = (0x60 << 1),
-            .atcai2c.bus            = 1, // /dev/i2c-1
-            .atcai2c.baud           = 100000,
-        },
-        .wake_delay                 = 1500,
-        .rx_retries                 = 20
-    };
-
     // initialize CryptoAuthLib for an ECC default I2C interface
-    if (atcab_init(&mycfg_i2c) != ATCA_SUCCESS) {
+    if (atcab_init(&atecc608_i2c) != ATCA_SUCCESS) {
        printf("Init error\n");
        exit(1);
     }
--- app-random/sha.c
+++ app-random/sha.c
@@ -7,28 +7,19 @@
  */
 
 
-#include "cryptoauthlib.h"
 #include <string.h>  // strlen
 #include <stdio.h>   // printf
+#include "cryptoauthlib.h"
+#include "atca_config.h"
 
 int main(void) {
-    uint8_t digest[32];
     uint8_t message[] = "0123456789";
-
-    ATCAIfaceCfg mycfg_i2c = {
-        .iface_type                 = ATCA_I2C_IFACE,
-        .devtype                    = ATECC608,
-        {
-            .atcai2c.address        = (0x60 << 1),
-            .atcai2c.bus            = 1, // /dev/i2c-1
-            .atcai2c.baud           = 100000,
-        },
-        .wake_delay                 = 1500,
-        .rx_retries                 = 20
-    };
+    uint8_t digest[32];
+    uint8_t buffer[1024];
+    size_t buffer_size = sizeof(buffer);
 
     // initialize CryptoAuthLib for an ECC default I2C interface
-    if (atcab_init(&mycfg_i2c) != ATCA_SUCCESS) {
+    if (atcab_init(&atecc608_i2c) != ATCA_SUCCESS) {
         printf("Init error\n");
         exit(1);
     }
@@ -38,10 +29,12 @@ int main(void) {
         exit(1);
     }
 
-    for(int i = 0; i < 32; i++) {
-        printf("%02x ", digest[i]);
-    }
-    printf("\n");
+    atcab_bin2hex(digest, sizeof(digest), buffer, &buffer_size);
+
+    puts("");
+    puts("SHA256 output:");
+    puts(buffer);
+    puts("");
 
 }
 
