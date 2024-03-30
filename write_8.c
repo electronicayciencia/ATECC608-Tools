010c5388d5fb7818c7531fdebb8345eec837ba20
Electronicayciencia <electronicayciencia@gmail.com>
2024-03-31 00:32:00 +0100

    Documentation

--- write_8.c
+++ write_8.c
@@ -11,13 +11,20 @@
 
 #define SLOT 8
 #define MAX_LENGTH 416
-#define DATA "EyC Original. 2024/03/22."
+//#define DATA "EyC Original. 2024/03/22."
 
-#define BLOCK_LEN 32
-
-int main(void) {
+int main(int argc, const char **argv) {
     ATCA_STATUS status;
     size_t start, data_length, offset_bytes;
+
+    if (argc != 2) {
+        printf("Write %d bytes to the slot %d.\n", MAX_LENGTH, SLOT);
+        printf("Usage %s <string>\n", argv[0]);
+        printf("Where:\n");
+        printf("    <string> is the data to write (%d bytes).\n", ATCA_BLOCK_SIZE);
+        printf("Ex.: %s \"EyC Original. 2024/03/22.\"\n", argv[0]);
+        exit(2);
+    }
 
     // initialize CryptoAuthLib for an ECC default I2C interface
     if (atcab_init(&cfg_atecc608_i2c) != ATCA_SUCCESS) {
@@ -26,25 +33,25 @@ int main(void) {
     }
 
 
-    data_length = strlen(DATA);
+    data_length = strlen(argv[1]);
     
-    for (start = 0; start < MAX_LENGTH; start += BLOCK_LEN) {
-        uint8_t buffer[BLOCK_LEN] = {0};
+    for (start = 0; start < MAX_LENGTH; start += ATCA_BLOCK_SIZE) {
+        uint8_t buffer[ATCA_BLOCK_SIZE] = {0};
 
         if (start < data_length) {
             size_t bytes_to_copy = data_length - start;
 
-            if (bytes_to_copy > BLOCK_LEN)
-                bytes_to_copy = BLOCK_LEN;
+            if (bytes_to_copy > ATCA_BLOCK_SIZE)
+                bytes_to_copy = ATCA_BLOCK_SIZE;
 
-            memcpy(buffer, DATA + start, bytes_to_copy);
+            memcpy(buffer, argv[1] + start, bytes_to_copy);
         }
 
         printf("%4d:  ", start);
         for (int i = 0; i <= 31; i++) printf("%02x ", buffer[i]);
         puts("");
         
-        status = atcab_write_bytes_zone(ATCA_ZONE_DATA, SLOT, start, buffer, BLOCK_LEN);
+        status = atcab_write_bytes_zone(ATCA_ZONE_DATA, SLOT, start, buffer, ATCA_BLOCK_SIZE);
 
         if (status != ATCA_SUCCESS) {
             printf("Slot write error: %d\n", status);
